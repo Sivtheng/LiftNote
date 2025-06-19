c1d86a1c6934b2a63d0980ad0f255f479168f96a
dynamo-sivtheng <sivtheng@m.dynamo-tech.com>
2025-06-19 09:42:31 +0700

    [Update] Modify .env-example for local development and configure DigitalOcean Spaces in filesystems. Remove unnecessary middleware from app.php.

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -48,7 +48,7 @@ return [
             'driver' => 's3',
             'key' => env('DO_SPACES_KEY'),
             'secret' => env('DO_SPACES_SECRET'),
-            'endpoint' => 'https://' . env('DO_SPACES_ENDPOINT'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
             'url' => env('DO_SPACES_URL'),
